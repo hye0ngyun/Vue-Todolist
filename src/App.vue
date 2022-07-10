42828f01e9f895a1b1fba5fa3443b27d68d0d497
Shin <shg7271@gmail.com>
2022-07-10 20:05:14 +0900

    Update: date function

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,36 @@
 <script setup>
+import { reactive } from "@vue/reactivity";
+import { computed } from "@vue/runtime-core";
 import TodoList from "./components/TodoList.vue";
+
+const state = reactive({
+  date: new Date(),
+});
+const currHours = computed(() => {
+  return state.date.getHours().toString().padStart(2, "0");
+});
+const currMinutes = computed(() => {
+  return state.date.getMinutes().toString().padStart(2, "0");
+});
+const currSeconds = computed(() => {
+  return state.date.getSeconds().toString().padStart(2, "0");
+});
+setInterval(() => {
+  state.date = new Date();
+  const hours = state.date.getHours().toString().padStart(2, "0");
+  const minutes = state.date.getMinutes().toString().padStart(2, "0");
+  const seconds = state.date.getSeconds().toString().padStart(2, "0");
+  return `${hours}<span>:</span>${minutes}<span>:</span>${seconds}`;
+}, 1000);
 </script>
 
 <template>
-  <TodoList />
+  <div class="wrap">
+    <h2 class="date">
+      {{ `${currHours}:${currMinutes}:${currSeconds}` }}
+    </h2>
+    <TodoList />
+  </div>
 </template>
 
 <style lang="scss">
@@ -11,5 +38,20 @@ body,
 html,
 #app {
   height: 100%;
+}
+</style>
+<style scoped>
+.wrap {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  align-items: center;
+  padding: 25px;
+}
+.date {
+  font-size: 3rem;
+  font-weight: bold;
 }
 </style>
